ce9a83dbf40c41d8ba00bfc5947aa8936f894753
Emilly Natacha <emilly123taurino@gmail.com>
2021-08-29 22:44:21 -0300

    Pequenas alterações

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "ola mundo"
+    usuario = {"nome":"Emilly", "sobrenome":"Natacha"}
+    return render_template('index.html', usuario=usuario, titulo='Página Inicial')
+
+@app.route('/outro')
+def outro():
+    return render_template('contato.html', titulo= "Contato")
